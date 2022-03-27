a055cd57cbfef51f606e0b31af976fec18cd8ff8
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-03-27 14:39:25 +0300

    Add project files.

---
+++ StokTakip.Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.Core.Entities
+{
+    public class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
---
+++ StokTakip.Entities/Kategori.cs
@@ -0,0 +1,14 @@
+﻿using StokTakip.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.Entities
+{
+    public class Kategori : BaseEntity
+    {
+
+    }
+}
